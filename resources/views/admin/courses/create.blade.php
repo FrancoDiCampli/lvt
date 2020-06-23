f6e0ac222eb41fa76cb93f1f0d0026c8d2345f75
francodicampli <francodicampli@gmail.com>
2020-06-23 18:33:08 -0300

    Courses and responsive

--- resources/views/admin/courses/create.blade.php
+++ resources/views/admin/courses/create.blade.php
@@ -2,44 +2,68 @@
 
 @section('content')
 
-<div class="container font-montserrat">
-    <div class="card  rounded-sm bg-gray-100 mx-auto mt-10 shadow-lg">
-        <div class="card-title bg-white p-0 w-full p-5 border-b flex items-center justify-between ">
-           <h1 class="text-teal-600 font-semibold">New Course</h1>
-           {{-- <a href="" class="bg-teal-600 text-white text-sm p-2 shadow-lg hover:text-gray-700">New Course</a> --}}
-
+<div class="container font-montserrat text-sm ">
+    <div class="card  rounded-sm bg-gray-100 mx-auto md:mt-10 shadow-lg">
+        <div class="card-title bg-white w-full p-1 md:p-5  border-b flex items-center justify-between md:justify-between ">
+           <h1 class="text-teal-600 font-semibold">Courses</h1>
         </div>
         <div class="card-body py-5">
             <form method="POST" action="{{ route('courses.store') }}" class="mx-auto" >
                 @csrf
-                <div class="flex flex-wrap">
-                    <div class="flex items-center border w-5/12 mx-auto h-10 bg-white">
-                        <select id="anio"  onchange="setCode()" name="year" id="" name="year" class="focus:outline-none">
-                            <option disabled selected value> -- select an option -- </option>
+                <div class="md:flex">
+                    <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                        Year
+                        </label>
+                        <div class="relative">
+                        <select id="anio"  onchange="setCode()" name="year" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                            <option disabled selected value> -- select a year -- </option>
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
                         </select>
-                    </div>
-
-                    <div class="flex items-center border w-5/12 mx-auto h-10 bg-white">
-                        <select  id="division" onchange="setCode()"  id="" name="division"  class="focus:outline-none">
-                            <option disabled selected value> -- select an option -- </option>
-                            <option value="A">A</option>
-                            <option value="B">B</option>
-                            <option value="C">C</option>
-                            <option value="D">D</option>
-                        </select>
+                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                        </div>
                     </div>
                 </div>
-                <div class="flex my-5">
-                    <div class="flex items-center border w-5/12 mx-auto h-10 bg-white">
-                        <input type="text" id="name" name="name" value="" class="bg-transparent focus:outline-none">
+
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+                      Division
+                    </label>
+                    <div class="relative">
+                      <select id="division" onchange="setCode()"  id="" name="division"  class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                        <option disabled selected value> -- select an option -- </option>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="C">C</option>
+                        <option value="D">D</option>
+                      </select>
+                      <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                      </div>
                     </div>
-                    <div class="flex items-center border w-5/12 mx-auto h-10 bg-white">
-                        <input id="code" type="text"  name="code" value="" class="bg-transparent focus:outline-none">
+                </div>
+                </div>
+
+                <div class="flex flex-wrap my-5">
+                    <div class="w-full md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                          Course
+                        </label>
+                        <input type="text" id="name" name="name" value="" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
                     </div>
+
+                    <div class="w-full md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                          Code
+                        </label>
+                        <input id="code" type="text"  name="code" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe">
+                    </div>
+
+
                 </div>
 
 
@@ -49,9 +73,7 @@
                     </span>
                 @enderror
 
-
-
-                <button type="submit" class="mb-5 font-semibold w-8/12 flex mx-auto text-center justify-center bg-teal-600 text-gray-200 px-5 py-2">Save</button>
+                <button type="submit" class="w-8/12 mb-5 font-semibold md:w-5/12 py-2 flex mx-auto  justify-center bg-teal-600 text-gray-200 ">Save</button>
 
             </form>
         </div>
--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -2,32 +2,32 @@
 
 @section('content')
 
-<div class="container font-montserrat">
-    <div class="card  rounded-sm bg-gray-100 mx-auto mt-10 shadow-lg">
-        <div class="card-title bg-white p-0 w-full p-5 border-b flex items-center justify-between ">
+<div class="container font-montserrat text-sm ">
+    <div class="card  rounded-sm bg-gray-100 mx-auto md:mt-10 shadow-lg">
+        <div class="card-title bg-white w-full p-1 md:p-5  border-b flex items-center justify-between md:justify-between ">
            <h1 class="text-teal-600 font-semibold">Courses</h1>
-        <a href="{{route('courses.create')}}" class="bg-teal-600 text-white text-sm p-2 shadow-lg hover:text-gray-700">New Course</a>
+            <a href="{{route('courses.create')}}" class="bg-teal-600 text-white text-sm p-2 shadow-lg hover:text-gray-700">New Course</a>
 
         </div>
-        <div class="card-body py-5">
+        <div class="card-body py-2">
             <table class="text-gray-700">
                 <thead>
                     <tr class="text-center">
-                        <th class="w-1/12 px-10">ID</th>
-                        <th class="w-5/12 px-10">Name</th>
-                        <th class="w-3/12 px-10">Code</th>
+                        <th class="w-1/12 px-10 hidden md:block">ID</th>
+                        <th class="md:w-5/12 w-2/12 px-10 ">Name</th>
+                        <th class="w-3/12 ">Code</th>
                         <th class="w-3/12 px-10">Actions</th>
 
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($courses as $course)
-                         <tr class="text-center">
-                         <td  class="w-1/12 px-10">{{$course->id}}</td>
-                        <td  class="w-5/12 ">{{$course->name}}</td>
+                        <tr class="text-center">
+                        <td  class="w-1/12 px-10 hidden md:block ">{{$course->id}}</td>
+                        <td  class="w-3/12 ">{{$course->name}}</td>
                         <td  class="w-3/12">{{$course->code}}</td>
-                        <td  class="w-3/12 px-10 ">
-                            <div class="flex">
+                        <td  class="md:w-3/12 w-9/12">
+                            <div class="flex justify-center">
                                 <a href="" class="mx-1 text-teal-400">
                                 <svg aria-hidden="true" data-prefix="fas" data-icon="pencil-alt" class="h-4 w-4 svg-inline--fa fa-pencil-alt fa-w-16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M497.9 142.1l-46.1 46.1c-4.7 4.7-12.3 4.7-17 0l-111-111c-4.7-4.7-4.7-12.3 0-17l46.1-46.1c18.7-18.7 49.1-18.7 67.9 0l60.1 60.1c18.8 18.7 18.8 49.1 0 67.9zM284.2 99.8L21.6 362.4.4 483.9c-2.9 16.4 11.4 30.6 27.8 27.8l121.5-21.3 262.6-262.6c4.7-4.7 4.7-12.3 0-17l-111-111c-4.8-4.7-12.4-4.7-17.1 0zM124.1 339.9c-5.5-5.5-5.5-14.3 0-19.8l154-154c5.5-5.5 14.3-5.5 19.8 0s5.5 14.3 0 19.8l-154 154c-5.5 5.5-14.3 5.5-19.8 0zM88 424h48v36.3l-64.5 11.3-31.1-31.1L51.7 376H88v48z"/></svg>
                                 </a>
